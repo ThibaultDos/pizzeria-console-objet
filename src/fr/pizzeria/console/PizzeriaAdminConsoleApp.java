ce718b5690a6cce99acf3c627189051b62531ba6
Thibault <thibault.dos-anjos@laposte.net>
2019-03-19 15:16:15 +0100

    tp 5 : ajout des fonctionnalités CRUD

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 import fr.pizzeria.model.Pizza;
 
-public class PizzeriaAdminConsoleApp 
-{
-	public static void main(String[] args) 
-	{
+public class PizzeriaAdminConsoleApp{
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args){
 		Pizza [] carte = new Pizza[8] ;
 		carte[0] = new Pizza ("PEP", "Pepperoni", 12.50);
 		carte[1] = new Pizza ("MAR", "Margherita", 14.00);
@@ -19,60 +20,100 @@ public class PizzeriaAdminConsoleApp
 		carte[7] = new Pizza ("IND", "L’indienne", 14.00);
 		
 		int choix=0;
-		while(choix!=99)
-		{
-			System.out.println("***** Pizzeria Administration *****");
-			System.out.println("1. Lister les pizzas");
-			System.out.println("2. Ajouter une nouvelle pizza");
-			System.out.println("3. Mettre à jour une pizza");
-			System.out.println("4. Supprimer une pizza");
-			System.out.println("99. Sortir ");
+		while(choix!=99){
+			System.out.println("***********************************");
+			System.out.println("***   Pizzeria Administration   ***");
+			System.out.println("***********************************");
+			System.out.println("*  1. Lister les pizzas           *");
+			System.out.println("*  2. Ajouter une nouvelle pizza  *");
+			System.out.println("*  3. Mettre à jour une pizza     *");
+			System.out.println("*  4. Supprimer une pizza         *");
+			System.out.println("*  99. Sortir                     *");
+			System.out.println("***********************************");
 			System.out.print(">>>");
 						
 			Scanner user = new Scanner(System.in);
 			choix = user.nextInt();
 			
-			if(choix==1)
-				{
-				System.out.println("Liste des pizze :");
+			if(choix==1){
+				System.out.println("***********************************");
+				System.out.println("**      Liste des pizze :        **");
+				System.out.println("***********************************");
 				
-				for(int i = 0; i<carte.length;i++)
-					{
-					afficheCarte(carte[i]);
+				for(int i = 0; i<carte.length;i++){
+					if (carte[i] != null){
+						afficheCarte(carte[i]);
+					}
+				}
+			}
+			else if(choix==2){
+				System.out.println("Ajout d'une nouvelle pizza");
+				System.out.print("Quel est le code de la nouvelle pizza ?\n>>>");
+				String code = user.next();
+				System.out.print("Et son libellé (nom de la pizza) ?\n>>>");
+				String libelle = user.next();
+				System.out.print("Combien coûte cette pizza ?\n>>>");
+				double prix = user.nextDouble();
+				Pizza addPizza = new Pizza(code, libelle, prix);
+				Pizza [] newCarte = new Pizza[carte.length+1] ;
+				for (int i=0;i<carte.length;i++){
+					newCarte[i]=carte[i];
 					}
 				
-				}
-			else if(choix==2)
-				{
-				System.out.println("Ajout d'une nouvelle pizza");
-				String code = user.next();
+				newCarte[newCarte.length-1] = addPizza;
+				carte = newCarte;
 				
-				Pizza piz8 = new Pizza ("IND", "L’indienne", 14.00);
 				}
-			else if(choix==3)
-				{
+			else if(choix==3){
 				System.out.println("Mise à jour d'une pizza");
+				System.out.print("Quelle pizza souhaitez-vous modifier ? (code à trois lettres)\n>>>");
+				String code = user.next();
+				System.out.print("Entrez le nouveau code à trois lettres S.V.P\n>>>");
+				String newCode = user.next();
+				System.out.print("Quel sera son nouveau libellé ? (nom de pizza sans espace)\n>>>");
+				String newLibelle = user.next();
+				System.out.print("Et son nouveau prix ?\n>>>");
+				double newPrix = user.nextDouble();
+				
+				for (int i=0;i< carte.length;i++){
+					if (carte[i].getCode().equals(code)){
+						carte[i].setCode(newCode);
+						carte[i].setLibelle(newLibelle);
+						carte[i].setPrix(newPrix);
+					}
 				}
-			else if(choix==4)
-				{
+				
+			}
+			else if(choix==4){
 				System.out.println("Suppression d'une pizza");
+				System.out.print("Quelle pizza souhaitez-vous supprimer ? (code à trois lettres)\n>>>");
+				String code = user.next();	
+				Pizza[] newCarte = new Pizza[carte.length] ;				
+				for (int j=0;j< carte.length;j++){
+					if (carte[j].getCode().equals(code)){
+						System.out.println("Adieu, pizza "+carte[j].getLibelle());
+						carte[j]=null;
+						
+//						for (int i=0;i<carte.length;i++){
+//							newCarte[i]=carte[i];							
+//						}	
+					}				
 				}
-			else if(choix==99)
-				{				
+			}
+			else if(choix==99){				
 				System.out.println("Au revoir \u2639");
 				}
 			
-			else
-				{
-				System.out.println("Faisez un choix valide, merci \u263A");
+			else{
+				System.out.println("Ce n'est pas un choix valide, try again \u263A");
 				}
 		}
 		
 	}
 	
-	public static void afficheCarte(Pizza pizza)
-	{
-		System.out.println(pizza.id+" "+pizza.code+" -> "+pizza.libelle+" ("+pizza.prix+"€)");
+	public static void afficheCarte(Pizza pizza){
+		System.out.println(pizza.getCode()+" -> "+pizza.getLibelle()+" ("+pizza.getPrix()+"€)");
+		
 	}
 
 }
--- src/fr/pizzeria/model/Pizza.java
+++ src/fr/pizzeria/model/Pizza.java
@@ -1,13 +1,39 @@
 package fr.pizzeria.model;
 
+/**
+ * @author 33689
+ *cette classe représente une pizza
+ */
 public class Pizza 
 {
-	public int id=0;
-	public String code=null;
-	public String libelle=null;
-	public double prix=0;
-	public static int cpt=0;
+	/**
+	 * id : identifiant unique
+	 */
+	private int id=0;
+	/**
+	 * code : code à trois lettres de la pizza
+	 */
+	private String code=null;
+	/**
+	 * libelle : nom de la pizza
+	 */
+	private String libelle=null;
+	/**
+	 * prix : prix en euros
+	 */
+	private double prix=0;
+	/**
+	 * cpt : compteur pour incrémenter l'id de chaque pizza disponible sur le menu
+	 */
+	private static int cpt=0;
 	
+	/**
+	 * constructeur pour créer une instance de la classe Pizza
+	 * @param id
+	 * @param code
+	 * @param libelle
+	 * @param prix
+	 */
 	public Pizza(int id, String code, String libelle, double prix) 
 	{
 		this.id=id;
@@ -16,6 +42,68 @@ public class Pizza
 		this.prix=prix;	
 	}
 	
+	/**
+	 * @return
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	/**
+	 * @param code
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getLibelle() {
+		return libelle;
+	}
+
+	/**
+	 * @param libelle
+	 */
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	/**
+	 * @return
+	 */
+	public double getPrix() {
+		return prix;
+	}
+
+	/**
+	 * @param prix
+	 */
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	/**
+	 * constructeur pour instancier une pizza qui sera affichée par le menu
+	 * @param code
+	 * @param libelle
+	 * @param prix
+	 */
 	public Pizza(String code, String libelle, double prix) 
 	{
 		this.code=code;
