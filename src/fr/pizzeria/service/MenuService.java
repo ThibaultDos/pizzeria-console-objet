ee8ec04cb58f20649dce211e8052cf6f671ae074
Thibault <thibault.dos-anjos@laposte.net>
2019-04-03 12:23:26 +0200

    conversion du projet en projet Maven

--- src/fr/pizzeria/service/MenuService.java
+++
@@ -1,9 +0,0 @@
-package fr.pizzeria.service;
-
-import java.util.Scanner;
-import fr.pizzeria.exception.StockageException;
-
-public abstract class  MenuService {
-	abstract void executeUC(Scanner scanner) throws StockageException;
-
-}
